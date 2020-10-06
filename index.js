cb6d27d3d64a48af42878a9257abe67d9c0f99f0
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-10-05 21:19:52 -0600

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-animal = ""
+let animal = ""
 
 function myAnimal() {
-  return animal
+    let animal = 'dog';
+    return animal
 }
 
 function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
-  return animal
+    // How can we make sure that this function
+    // and the above function both pass?
+    // P.S.: You can't just hard-code 'cat' below
+    let animal = 'cat';
+    return animal
 }
 
 function add2(n) {
-  return n + two
+    let two = 2
+    return n + two
 
-  // Feel free to move things around!
-  two = 2
+    // Feel free to move things around!
+
 }
